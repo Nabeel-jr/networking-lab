6a0b5058ea4ae77e6c31d51816c625769f24dfcb
Nabeel-jr <145667291+Nabeel-jr@users.noreply.github.com>
2024-04-02 13:33:58 +0530

    Add files via upload

---
+++ april 1/arrpal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -p "enter string: " n
+len=${#n}
+rev=""
+for ((i=$len-1;i>=0;i--))
+do
+	rev="$rev${n:$i:1}"
+done
+if [ $n == $rev ]
+then
+	echo "$n is palindrom"
+else
+	echo "not palindrom"
+fi
